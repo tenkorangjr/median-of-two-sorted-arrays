fa89b3e6e3c1b231d4aa6bab2e4fffc6c181700f
Michael Tenkorang Jr <82475246+tenkorangjr@users.noreply.github.com>
2023-03-16 23:26:03 -0400

    Updated

--- MedianHunter.java
+++ MedianHunter.java
@@ -1,45 +1,66 @@
-/**
- * Represents a utility class to find the median of two sorted arrays.
- */
 public class MedianHunter {
 
     /**
      * Returns the median of the combined arrays {@code sortedArray1} and
-     * {@code sortedArray2}.
-     *
+     * {@code sortedArray2}
+     * 
      * @param sortedArray1 the first sorted array
      * @param sortedArray2 the second sorted array
-     * @return the median of the combined arrays
+     * @return the median of the combined arrays.
      */
     public static double findMedian(int[] sortedArray1, int[] sortedArray2) {
-        /*
-         * Find the median of two sorted arrays using binary search
-         */
 
-        int m = sortedArray1.length;
+        int size = sortedArray1.length;
 
-        // Initialize variables for binary search
-        int iMinimum = 0;
-        int iMaximum = m;
+        int iMin = 0;
+        int iMax = sortedArray1.length - 1;
+        int jMin = 0;
+        int jMax = sortedArray2.length - 1;
 
-        // Perform binary search
-        while (iMinimum <= iMaximum) {
-            int i = (iMinimum + iMaximum) / 2;
-            int j = i;
+        while (iMax - iMin > 1 && jMax - jMin > 1) {
+            if ((size) % 2 == 0) {
+                int iMid = (iMax + iMin) / 2;
+                int jMid = (jMax + jMin) / 2;
 
-            if (i < m && sortedArray2[j - 1] > sortedArray1[i]) {
-                iMinimum = i + 1;
-            } else if (i > 0 && sortedArray1[i - 1] > sortedArray2[j]) {
-                iMaximum = i - 1;
+                if (sortedArray1[iMid] < sortedArray2[jMid]) {
+                    iMin = iMid;
+                    jMax = jMid + 1;
+                } else {
+                    iMax = iMid + 1;
+                    jMin = jMid;
+                }
+                size -= (iMid - iMin);
+
             } else {
-                int maxLeft = sortedArray2[j - 1];
-                int minRight = sortedArray2[j];
+                int iMid = (iMax + iMin) / 2;
+                int jMid = (jMax + jMin) / 2;
 
-                return (maxLeft + minRight) / 2.0;
+                if (sortedArray1[iMid] < sortedArray2[jMid]) {
+                    iMin = iMid;
+                    jMax = jMid;
+                } else {
+                    iMax = iMid;
+                    jMin = jMid;
+                }
+                size -= (iMid - iMin);
             }
         }
 
-        return 0.0;
+        int max;
+        int min;
+
+        if (sortedArray1[iMin] > sortedArray2[jMin]) {
+            max = sortedArray1[iMin];
+        } else {
+            max = sortedArray2[jMin];
+        }
+
+        if (sortedArray1[iMax] < sortedArray2[jMax]) {
+            min = sortedArray1[iMax];
+        } else {
+            min = sortedArray2[jMax];
+        }
+        return (max + min) / 2.0;
     }
 
     public static void main(String[] args) {
